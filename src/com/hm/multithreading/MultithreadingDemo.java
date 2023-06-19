2e076ccbbf38d4d0b1f8efaded573424d2adad38
HAPPIESTMINDS\Kunte.keshava <kunte.keshava@happiestminds.com>
2023-06-19 16:33:02 +0530

    commit from feature-1 branch

--- src/com/hm/multithreading/MultithreadingDemo.java
+++ src/com/hm/multithreading/MultithreadingDemo.java
@@ -11,6 +11,7 @@ public class MultithreadingDemo {
 			public void run() {
 //				System.out.println("We are now in thread: "+Thread.currentThread().getName());
 //				System.out.println("Current thread priority is "+Thread.currentThread().getPriority());
+				System.out.println("Hello feature 1");
 				throw new RuntimeException("Intentional exception");				
 			}
 		});
